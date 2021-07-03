74bb0e2270057050233a01b376e035d22413499d
joji <joji.wesley@hotmail.com>
2021-07-02 23:33:38 -0300

    Revert "asd"
    This reverts commit 25567a44bd099dc8094d699ccd6b9206a0f86473.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,3 @@
-import { useHistory } from 'react-router-dom'; // importação do metodo de navegação.
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -8,12 +6,6 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
 export function Home(){
-
-  const history = useHistory();    // metodo de navegação entre as paginas. 
-  
-  function handleCreateRoom() { // funçaó de navegação 
-    history.push('/rooms/new')
-  }
   return (
     <div id="page-auth">
       <aside>
@@ -24,7 +16,7 @@ export function Home(){
       <main>
         <div className="main-content">  
         <img src={logoImg} alt="Letmeask" />
-          <button onClick={handleCreateRoom} className="create-room">
+          <button className="create-room">
             <img src={googleIconImg} alt="Logo da Google" />
             Crie sua sala com a Google 
           </button>
